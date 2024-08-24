a65d817cc986f2035e73d49d66aa1b86a31bfbeb
Zafor Iqbal <xoy4444@gmail.com>
2024-08-25 02:18:32 +0600

    jjjj

--- src/components/Preparation.js
+++ src/components/Preparation.js
@@ -11,7 +11,7 @@ const mapStateToProps = (state) => {
 function Preparation(props) {
   const darkTheme = createTheme({
     palette: {
-      mode: props.mode,
+      mode: props.mode ||"light"
     },
   });
 
