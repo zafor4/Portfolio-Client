8985f048d7c83185718c3b3cef3b1d1f72d02a4b
Zafor Iqbal <xoy4444@gmail.com>
2024-08-26 15:30:53 +0600

    article fixed

--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -22,7 +22,7 @@ const Articles = (props) => {
   return (
     <Layout title='Articles' className='container mx-auto px-4 md:px-20'>
 <Preparation>
-       <div className='mt-20 mb-20 w-3/4'>
+       <div className='mt-20 mb-20 md:w-3/4'>
        <h1 className='text-4xl font-bold'>Crafting Code, Building Futures, and Reaching for the Stars: Articles by Zafor Iqbal</h1>
        <p className='mt-4'>All of my thoughts on programming, software engineering, web development and its architectures.</p>
        <div>
