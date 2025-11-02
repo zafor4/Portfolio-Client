8cb1758a328291c7cbe0a861144eda861d3c20ab
Zafor Iqbal <iqbal15-5128@diu.edu.bd>
2025-11-02 19:57:08 +0600

    azure addded

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,10 @@
 import { jwtDecode } from "jwt-decode"
 
-const dev='http://localhost:3001/api'
-export const baseUrl=dev
+// const dev='http://localhost:3001/api'
+// export const baseUrl=dev
 
-// export const baseUrl='https://portfolioserver-v2b4.onrender.com/api'
+export const baseUrl =
+  "https://zaforportfolioserver-bcfpgqejc3cad0h7.centralindia-01.azurewebsites.net/api";
 
 export const decoded=token=>{
     const user=jwtDecode(token)
